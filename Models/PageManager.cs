964e28c258165ea737d8087c6c71269204f3167d
IDerkBot <ivanderk2003@gmail.com>
2022-05-05 01:40:34 +0300

    Создал класс для перемещения между страницами

---
+++ Models/PageManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Controls;
+
+namespace VetClinic.Models
+{
+    internal class PageManager
+    {
+		private static Frame MainFrame { get; set; }
+        public static void SetFrame(Frame frame)
+        {
+            MainFrame = frame;
+        }
+        public static void Navigate(Page moveToPage)
+        {
+            MainFrame.Navigate(moveToPage);
+        }
+        public static void GoBack()
+        {
+            if (MainFrame.CanGoBack)
+                MainFrame.GoBack();
+        }
+        public static Page GetPage()
+        {
+            return MainFrame.Content as Page;
+        }
+	}
+}
