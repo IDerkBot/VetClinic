1bf1ef19428fd104306bebe161d6c7f723c510f0
IDerkBot <ivanderk2003@gmail.com>
2022-05-07 09:52:51 +0300

    Сделал редактирование\добавление породы

--- Views/Pages/EditPages/TypeBreedEditPage.xaml.cs
+++ Views/Pages/EditPages/TypeBreedEditPage.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using VetClinic.Models.Entity;
+using MessageBox = System.Windows.MessageBox;
 
 namespace VetClinic.Views.Pages.EditPages
 {
@@ -20,9 +11,46 @@ namespace VetClinic.Views.Pages.EditPages
     /// </summary>
     public partial class TypeBreedEditPage : Page
     {
+        private TypeBreed _currentTypeBreed;
         public TypeBreedEditPage()
         {
             InitializeComponent();
+            CbTypesAnimal.ItemsSource = VeterinaryEntities.GetContext().TypeAnimals.ToList();
+            _currentTypeBreed = new TypeBreed();
+            DataContext = _currentTypeBreed;
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            _currentTypeBreed.Title = TbTitle.Text;
+            if (_currentTypeBreed.ID == 0) VeterinaryEntities.GetContext().TypeBreeds.Add(_currentTypeBreed);
+            VeterinaryEntities.GetContext().SaveChanges();
+            MessageBox.Show("Данные сохранены!");
+        }
+
+        private bool _isEdit;
+        private void LvItems_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (!_isEdit) return;
+            _currentTypeBreed = (TypeBreed)LvItems.SelectedItem;
+            DataContext = _currentTypeBreed;
+        }
+
+        private void CbTypesAnimal_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (LvItems.SelectedItem != null)
+            {
+                _currentTypeBreed = new TypeBreed
+                {
+                    TypeAnimal = CbTypesAnimal.SelectedItem as TypeAnimal
+                };
+                //DataContext = _currentTypeBreed;
+                _isEdit = false;
+                LvItems.SelectedItem = null;
+            }
+            var typeAnimal = (CbTypesAnimal.SelectedItem as TypeAnimal)?.Title;
+            LvItems.ItemsSource = VeterinaryEntities.GetContext().TypeBreeds.Where(x => x.TypeAnimal.Title == typeAnimal).ToList();
+            _isEdit = true;
         }
     }
 }
