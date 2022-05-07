586772afbd1795560ee1044160e9718a8e49b6a8
IDerkBot <ivanderk2003@gmail.com>
2022-05-07 05:58:02 +0300

    Очистил страницу и сделал выборку по типу

--- Views/Pages/PetPage.xaml.cs
+++ Views/Pages/PetPage.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using VetClinic.Models;
 using VetClinic.Models.Entity;
 using VetClinic.Views.Pages.EditPages;
@@ -66,7 +55,9 @@ namespace VetClinic.Views.Pages
 
         private void CbTypes_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            var list = VeterinaryEntities.GetContext().Animals
+                .Where(x => x.TypeAnimal.Title == CbTypes.SelectedItem.ToString()).ToList();
+            DgPets.ItemsSource = list;
         }
 
         #endregion
