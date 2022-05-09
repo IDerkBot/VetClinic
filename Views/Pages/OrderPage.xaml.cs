056c36b6c8ed3b8390d5b3c6e59017795f989801
IDerkBot <ivanderk2003@gmail.com>
2022-05-09 22:38:09 +0300

    Сделал страницу вывода данных для заказов

--- Views/Pages/OrderPage.xaml.cs
+++ Views/Pages/OrderPage.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using VetClinic.Models;
+using VetClinic.Models.Entity;
+using VetClinic.Views.Pages.EditPages;
 
 namespace VetClinic.Views.Pages
 {
@@ -24,5 +16,45 @@ namespace VetClinic.Views.Pages
         {
             InitializeComponent();
         }
+
+        private void OrderPage_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            DgOrders.ItemsSource = VeterinaryEntities.GetContext().Orders.ToList();
+        }
+
+        #region События на нажатие кнопок
+
+        private void BtnDelete_OnClick(object sender, RoutedEventArgs e)
+        {
+            var items = DgOrders.SelectedItems.Cast<Order>().ToList();
+            if (MessageBox.Show($"Вы действительно хотите удалить {items.Count} элементов", "Удаление", MessageBoxButton.YesNo, MessageBoxImage.Question) != MessageBoxResult.Yes) return;
+            VeterinaryEntities.GetContext().Orders.RemoveRange(items);
+            VeterinaryEntities.GetContext().SaveChanges();
+            MessageBox.Show("Данные удалены!");
+        }
+
+        private void BtnAdd_OnClick(object sender, RoutedEventArgs e) => PageManager.Navigate(new OrderEditPage());
+
+        private void BtnEdit_OnClick(object sender, RoutedEventArgs e) => PageManager.Navigate(new OrderEditPage((sender as Button)?.DataContext as Order));
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            TbSearch.Text = "";
+            DgOrders.ItemsSource = VeterinaryEntities.GetContext().Orders.ToList();
+        }
+
+        private void BtnMore_OnClick(object sender, RoutedEventArgs e) => PageManager.Navigate(new OrderInfoPage((sender as Button)?.DataContext as Order));
+
+        #endregion
+
+        #region События изменений
+
+        private void TbSearch_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var searchText = TbSearch.Text;
+            //DgOrders.ItemsSource = VeterinaryEntities.GetContext().Orders.Where(x => x.Fullname.ToLower().Contains(searchText)).ToList();
+        }
+
+        #endregion
     }
 }
