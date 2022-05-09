d04665e14db09c845ace09ec75966d959641d931
IDerkBot <ivanderk2003@gmail.com>
2022-05-09 22:35:42 +0300

    Добавил классы для работы с файлом конфигурации

---
+++ Models/Config.cs
@@ -0,0 +1,19 @@
+﻿namespace VetClinic.Models
+{
+	internal class Config
+	{
+		public string Login { get; set; }
+		public string Password { get; set; }
+		public bool RememberMe { get; set; }
+		public Config()
+		{
+
+		}
+		public Config(string login, string password, bool remember)
+		{
+			Login = login;
+			Password = password;
+			RememberMe = remember;
+		}
+	}
+}
---
+++ Models/Data.cs
@@ -0,0 +1,9 @@
+﻿namespace VetClinic.Models
+{
+	internal class Data
+	{
+		public static string CurrentDirectory { get; set; } = "VeterinaryClinic";
+		public static string CurrentConfigFile { get; set; } = "config";
+		public static string CurrentConfigExtension { get; set; } = "json";
+	}
+}
