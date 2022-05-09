13dc82aefe501a6a19ffce77d0c4093f8fc301c7
IDerkBot <ivanderk2003@gmail.com>
2022-05-09 22:38:26 +0300

    Сделал страницу подробнее для заказа

---
+++ Views/Pages/OrderInfoPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using VetClinic.Models.Entity;
+
+namespace VetClinic.Views.Pages
+{
+    /// <summary>
+    /// Interaction logic for OrderInfoPage.xaml
+    /// </summary>
+    public partial class OrderInfoPage : Page
+    {
+        private readonly Order _currentOrder;
+        public OrderInfoPage(Order selectedOrder)
+        {
+            InitializeComponent();
+            _currentOrder = selectedOrder;
+            DataContext = _currentOrder;
+        }
+
+        private void OrderInfoPage_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            LvServices.ItemsSource = VeterinaryEntities.GetContext().Orders.Single(x => x.ID == _currentOrder.ID)
+                .Services.ToList();
+            LvMedications.ItemsSource = VeterinaryEntities.GetContext().Orders.Single(x => x.ID == _currentOrder.ID)
+                .Medications.ToList();
+        }
+    }
+}
