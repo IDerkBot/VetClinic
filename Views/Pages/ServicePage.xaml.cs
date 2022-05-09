173f11f21fc714ca120a394ef358f1a35f18cbab
IDerkBot <ivanderk2003@gmail.com>
2022-05-09 22:37:55 +0300

    Сделал страницу вывода данных для услуг

--- Views/Pages/ServicePage.xaml.cs
+++ Views/Pages/ServicePage.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using VetClinic.Models;
+using VetClinic.Models.Entity;
+using VetClinic.Views.Pages.EditPages;
 
 namespace VetClinic.Views.Pages
 {
@@ -24,5 +16,43 @@ namespace VetClinic.Views.Pages
         {
             InitializeComponent();
         }
+
+        private void ServicePage_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            DgService.ItemsSource = VeterinaryEntities.GetContext().Services.ToList();
+        }
+
+        #region События на нажатие кнопок
+
+        private void BtnDelete_OnClick(object sender, RoutedEventArgs e)
+        {
+            var items = DgService.SelectedItems.Cast<Service>().ToList();
+            if (MessageBox.Show($"Вы действительно хотите удалить {items.Count} элементов", "Удаление", MessageBoxButton.YesNo, MessageBoxImage.Question) != MessageBoxResult.Yes) return;
+            VeterinaryEntities.GetContext().Services.RemoveRange(items);
+            VeterinaryEntities.GetContext().SaveChanges();
+            MessageBox.Show("Данные удалены!");
+        }
+
+        private void BtnAdd_OnClick(object sender, RoutedEventArgs e) => PageManager.Navigate(new ServiceEditPage());
+
+        private void BtnEdit_OnClick(object sender, RoutedEventArgs e) => PageManager.Navigate(new ServiceEditPage((sender as Button)?.DataContext as Service));
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            TbSearch.Text = "";
+            DgService.ItemsSource = VeterinaryEntities.GetContext().Clients.ToList();
+        }
+
+        #endregion
+
+        #region События изменений
+
+        private void TbSearch_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var searchText = TbSearch.Text;
+            DgService.ItemsSource = VeterinaryEntities.GetContext().Services.Where(x => x.Title.ToLower().Contains(searchText)).ToList();
+        }
+
+        #endregion
     }
 }
